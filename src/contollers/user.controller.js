c351e9360506b9b8904d923af747c65a87fc69d3
himesh <himeshgotani858@gmail.com>
2026-06-14 19:23:57 +0530

    minor debugging

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser=asynchandler(async (req,res)=>{
     // return res
 
     const {fullName, email, username, password } = req.body
-    console.log(email,password);
+    // console.log(email,password);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -31,12 +31,17 @@ const registerUser=asynchandler(async (req,res)=>{
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
+    // console.log(req.files);
     
 
-     const avatarLocalPath = req.files?.avatar[0]?.path;
-     const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath 
+    // = req.files?.coverImage[0]?.path;
     
-
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
       if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,9 +16,9 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            lowecase: true,
-            trim: true, 
-        },
+            lowercase: true,
+            trim: true,
+},
         fullName: {
             type: String,
             required: true,
@@ -52,12 +52,11 @@ const userSchema = new Schema(
     }
 )
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
